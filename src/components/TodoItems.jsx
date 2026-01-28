99d1802f3f37dd25112d1d369f9d288810590910
Adarsh Tiwari <adarsh03082002@gmail.com>
2026-01-28 14:14:12 +0530

    Fix cross icon alignment and UI issues

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,45 +1,51 @@
 import './CSS/TodoItems.css'
-import tick from './assets/tick.png';
-import not_tick from './assets/not_tick.png';
-import cross from './assets/cross.png';
+import tick from './assets/tick.png'
+import not_tick from './assets/not_tick.png'
+import cross from './assets/cross.png'
 
+const TodoItems = ({ no, display, text, setTodos }) => {
 
-
-
-const TodoItems = ({no,display,text,setTodos}) => {
-
-  const deleteTodo = (no) =>{
-      let data = JSON.parse(localStorage.getItem("todos"));
-      data = data.filter((todo)=> todo.no!==no)
-      setTodos(data);
+  const deleteTodo = (no) => {
+    let data = JSON.parse(localStorage.getItem("todos")) || []
+    data = data.filter((todo) => todo.no !== no)
+    setTodos(data)
+    localStorage.setItem("todos", JSON.stringify(data))
   }
 
+  const toggle = (no) => {
+    let data = JSON.parse(localStorage.getItem("todos")) || []
 
-const toggle = () => {
-  let data = JSON.parse(localStorage.getItem("todos"));
-  for(let i = 0; i< data.length; i++){
-    if(data[i].no===no){
-      if(data[i].display===""){
-        data[i].display = "line-through";
-      }else{
-        data[i].display = "";
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].no === no) {
+        data[i].display = data[i].display === "" ? "line-through" : ""
+        break
       }
-      break;
-  }
-}
-   setTodos(data);
-}
+    }
 
+    setTodos(data)
+    localStorage.setItem("todos", JSON.stringify(data))
+  }
 
   return (
     <div className='todoitems'>
-        <div className={`todoitems-container ${display}`} onClick={()=>{toggle(no)}}>
-          {display===""?<img src={not_tick} alt=""/>:<img src={tick} alt=""/> }
-        
-           
-            <div className="todoitems-text">{text}</div>
-        </div>
-        <img className='todoitems-cross-icon' onClick={()=>{deleteTodo(no)}} src={cross} alt=""/>
+      <div
+        className={`todoitems-container ${display}`}
+        onClick={() => toggle(no)}
+      >
+        {display === ""
+          ? <img src={not_tick} alt="" />
+          : <img src={tick} alt="" />
+        }
+
+        <div className="todoitems-text">{text}</div>
+      </div>
+
+      <img
+        className='todoitems-cross-icon'
+        onClick={() => deleteTodo(no)}
+        src={cross}
+        alt=""
+      />
     </div>
   )
 }

f7862c863129e00c5f3e38d31e90887f3d5ff511
Adarsh Tiwari <adarsh03082002@gmail.com>
2026-01-29 00:14:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: "/Todo-List/",
+  base: "/TodoList/",
   plugins: [react()],
 })
